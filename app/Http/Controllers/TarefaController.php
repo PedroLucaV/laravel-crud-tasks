29471afdef3d67f5109707ccdeb9c9db1ea2840b
Pedro Lucas <izielucas2016@gmail.com>
2025-07-03 16:42:28 -0300

    feat: created controller for task

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tarefa;
 use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Tarefa::all());
+    }
+
+    public function store(Request $request)
+    {
+        $tarefa = Tarefa::create($request->all());
+        return response()->json($tarefa, 201);
+    }
+
+    public function show($id)
+    {
+        $tarefa = Tarefa::findOrFail($id);
+        return response()->json($tarefa);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $tarefa = Tarefa::findOrFail($id);
+        $tarefa->update($request->all());
+        return response()->json($tarefa);
+    }
+
+    public function destroy($id)
+    {
+        $tarefa = Tarefa::findOrFail($id);
+        $tarefa->delete();
+        return response()->json(['message' => 'Tarefa deletada']);
+    }
 }
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TarefaController;
+
+Route::get('/ping', function () {
+    return response()->json(['pong' => true]);
+});
+Route::get('/tarefas', [TarefaController::class, 'index']);
+Route::post('/tarefas', [TarefaController::class, 'store']);
+Route::get('/tarefas/{id}', [TarefaController::class, 'show']);
+Route::put('/tarefas/{id}', [TarefaController::class, 'update']);
+Route::delete('/tarefas/{id}', [TarefaController::class, 'destroy']);
