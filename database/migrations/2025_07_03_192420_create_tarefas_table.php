43006b71911dfd640906041ee803b78d4dae9922
Pedro Lucas <izielucas2016@gmail.com>
2025-07-03 16:26:50 -0300

    feat: created migration to task

--- database/migrations/2025_07_03_192420_create_tarefas_table.php
+++ database/migrations/2025_07_03_192420_create_tarefas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tarefas', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->text('descricao')->nullable();
+            $table->boolean('concluida')->default(false);
             $table->timestamps();
         });
     }
